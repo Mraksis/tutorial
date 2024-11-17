31e0c37ed953004c660b5ee288d7396a1ca7594c
Denis <denic-chumakov@mail.ru>
2024-11-17 21:38:51 +0300

    Task 177214

--- lichnyy-kabinet-pokupatelya/index.php
+++ lichnyy-kabinet-pokupatelya/index.php
@@ -1,4 +1,20 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Личный кабинет покупателя");
-?>Text here....<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.profile",
+	"",
+Array()
+);?><?$APPLICATION->IncludeComponent(
+	"bitrix:subscribe.form", 
+	".default", 
+	array(
+		"CACHE_TIME" => "3600",
+		"CACHE_TYPE" => "A",
+		"PAGE" => "#SITE_DIR#about/subscr_edit.php",
+		"SHOW_HIDDEN" => "N",
+		"USE_PERSONALIZATION" => "Y",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
